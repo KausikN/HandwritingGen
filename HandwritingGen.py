78b4701357edecf40da20e84a7f2cb6039b179d4
Kausik N <nkausik1999@gmail.com>
2021-05-17 17:39:13 +0530

    removed pywhatkit dependency

--- HandwritingGen.py
+++ HandwritingGen.py
@@ -4,7 +4,15 @@ Library Codes for HandwritingGen
 
 # Imports
 import cv2
-from pywhatkit import text_to_handwriting
+import requests
+
+# Module Functions
+def text_to_handwriting(string: str, save_to: str = "pywhatkit.png", rgb: list = [0, 0, 138]) -> None:
+    data = requests.get(
+        "https://pywhatkit.herokuapp.com/handwriting?text=%s&rgb=%s,%s,%s" % (string, rgb[0], rgb[1], rgb[2])).content
+    file = open(save_to, "wb")
+    file.write(data)
+    file.close()
 
 # Util Functions
 def Hex_to_RGB(val):
